48ae1a6b34a2d4376c18ac664f96e29fefb05faf
yaxin.wang <yaxin.wang@rea-group.com>
2020-02-15 18:45:39 +0800

    [Fibonacci] Modify fibonacci calcute type to avoid overflow problem.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,12 +1,12 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
-    public int calculate(int position) {
+    public long calculate(int position) {
         if (position == 1 || position == 2)
             return position;
 
-        int first = 1, second = 1;
-        int result = 0;
+        long first = 1, second = 1;
+        long result = 0;
         for (int x = 3; x <= position; x++) {
             result = first + second;
             first = second;
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -17,21 +17,26 @@ public class FibonacciTest {
 
     @Test
     void should_return_1_when_calculate_give_position_is_1() {
-        assertThat(fibonacci.calculate(1), is(1));
+        assertThat(fibonacci.calculate(1), is(1L));
     }
 
     @Test
     void should_return_2_when_calculate_give_position_is_3() {
-        assertThat(fibonacci.calculate(3), is(2));
+        assertThat(fibonacci.calculate(3), is(2L));
     }
 
     @Test
     void should_return_3_when_calculate_give_position_is_4() {
-        assertThat(fibonacci.calculate(4), is(3));
+        assertThat(fibonacci.calculate(4), is(3L));
     }
 
     @Test
     void should_return_144_when_calculate_give_position_is_12() {
-        assertThat(fibonacci.calculate(12), is(144));
+        assertThat(fibonacci.calculate(12), is(144L));
+    }
+
+    @Test
+    void should_return_12586269025L_when_calculate_give_position_is_50() {
+        assertThat(fibonacci.calculate(50), is(12586269025L));
     }
 }
