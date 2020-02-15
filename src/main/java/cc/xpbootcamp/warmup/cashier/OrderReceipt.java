a23d7992da9770c51080bd1679a1833ae4099c69
yaxin.wang <yaxin.wang@rea-group.com>
2020-02-15 19:43:55 +0800

    [Cachier] Extract constants in OrderReceipt.

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,6 +1,11 @@
 package cc.xpbootcamp.warmup.cashier;
 
 class OrderReceipt {
+    public static final String PRINTING_ORDERS_TITLE = "======Printing Orders======\n";
+    public static final String SALES_TAX = "Sales Tax";
+    public static final String TOTAL_AMOUNT = "Total Amount";
+    public static final double TAX_Rate = .10;
+
     private Order order;
 
     OrderReceipt(Order order) {
@@ -10,7 +15,7 @@ class OrderReceipt {
     String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        output.append("======Printing Orders======\n");
+        output.append(PRINTING_ORDERS_TITLE);
 
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
@@ -18,22 +23,18 @@ class OrderReceipt {
         double totalSalesTax = 0d;
         double totalAmount = 0d;
         for (Product product : order.getProducts()) {
-            output.append(product.getDescription());
-            output.append('\t');
-            output.append(product.getPrice());
-            output.append('\t');
-            output.append(product.getQuantity());
-            output.append('\t');
-            output.append(product.totalAmount());
-            output.append('\n');
+            output.append(product.getDescription()).append('\t');
+            output.append(product.getPrice()).append('\t');
+            output.append(product.getQuantity()).append('\t');
+            output.append(product.totalAmount()).append('\n');
 
-            double salesTax = product.totalAmount() * .10;
+            double salesTax = product.totalAmount() * TAX_Rate;
             totalSalesTax += salesTax;
             totalAmount += product.totalAmount() + salesTax;
         }
 
-        output.append("Sales Tax").append('\t').append(totalSalesTax);
-        output.append("Total Amount").append('\t').append(totalAmount);
+        output.append(SALES_TAX).append('\t').append(totalSalesTax);
+        output.append(TOTAL_AMOUNT).append('\t').append(totalAmount);
         return output.toString();
     }
 }
