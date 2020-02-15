d088637681bdbb42a70f6d16678c975880220f1e
yaxin.wang <yaxin.wang@rea-group.com>
2020-02-15 19:31:46 +0800

    [Cachier] Remove useless comment in OrderReceipt.

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,12 +1,5 @@
 package cc.xpbootcamp.warmup.cashier;
 
-/**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
- *
- */
 public class OrderReceipt {
     private Order order;
 
@@ -17,16 +10,11 @@ public class OrderReceipt {
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        // print headers
         output.append("======Printing Orders======\n");
 
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
 
-        // prints lineItems
         double totSalesTx = 0d;
         double tot = 0d;
         for (Product product : order.getLineItems()) {
@@ -39,18 +27,14 @@ public class OrderReceipt {
             output.append(product.totalAmount());
             output.append('\n');
 
-            // calculate sales tax @ rate of 10%
             double salesTax = product.totalAmount() * .10;
             totSalesTx += salesTax;
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += product.totalAmount() + salesTax;
         }
 
-        // prints the state tax
         output.append("Sales Tax").append('\t').append(totSalesTx);
 
-        // print total amount
         output.append("Total Amount").append('\t').append(tot);
         return output.toString();
     }
