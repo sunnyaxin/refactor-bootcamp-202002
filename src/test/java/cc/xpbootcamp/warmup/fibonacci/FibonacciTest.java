7914d18c3ce91bc61c1895334d5305e73318aac6
yaxin.wang <yaxin.wang@rea-group.com>
2020-02-15 17:28:33 +0800

    [Fibonacci] Add first test to return 1 when calculate give position is 1.

---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -0,0 +1,22 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FibonacciTest {
+
+    Fibonacci fibonacci;
+
+    @BeforeEach
+    void setUp() {
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    void should_return_1_when_calculate_give_position_is_1() {
+        assertThat(1, is(fibonacci.calculate(1)));
+    }
+}
